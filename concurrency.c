edd28ea8830973ce6ef43e489b7caa62aa654af8
AshleyJisooKim <ashleyk9318@gmail.com>
2019-05-21 11:38:49 -0700

    items

--- concurrency.c
+++ concurrency.c
@@ -6,7 +6,8 @@
 
 int buffer = 0;
 int choice[2];
-//int quantity[2];
+int quantity[2];
+char* food;
 
 pthread_mutex_t mutex=PTHREAD_MUTEX_INITIALIZER;	// mutex lock to avoid race conditions
 
@@ -14,29 +15,20 @@ void *producer(void *vargp){						// Producer Thread Function
 	int my = (int)vargp;
 	while(1){
 		pthread_mutex_lock(&mutex);		// Check if resource is free.
-		if(buffer < BUFFER){			// Check if the buffer is not full.
-			buffer++;
-			char* food;
-			for(int i = 0; i < 2; i++) {
-				do {
-					choice[i] = (rand() % 3);
-					
-					if(choice[i] == 0)
-						food = "hamburger";
-					else if(choice[i] == 1)
-						food = "fries";
-					else
-						food = "soda";
-					
-					///quantity[i] = (rand() % 10);
-				} while(i > 0 && (choice[i] == choice[i-1]/* || quantity[i] == 0*/));
-				printf("The chef produced %s.\n", food);
-			}
-			printf("Buffer Incremented^ \tby Cook: %d \tCurrent Size: %d\n",(my+1),buffer);
-			//sleep(1);
-		}
-		else{
-			// printf("Buffer OVERFLOW \t\t\tCurrent Size: %d\n",buffer );
+		for(int i = 0; i < 2; i++) {
+			do {
+				choice[i] = (rand() % 3);
+				
+				if(choice[i] == 0)
+					food = "hamburger(s)";
+				else if(choice[i] == 1)
+					food = "fries";
+				else
+					food = "soda";
+				
+				quantity[i] = (rand() % 10);
+			} while(i > 0 && (choice[i] == choice[i-1] || quantity[i] == 0));
+			printf("The chef produced %d %s.\n", quantity[i], food);
 		}
 		pthread_mutex_unlock(&mutex);	// free the resource.
 	}
@@ -45,33 +37,46 @@ void *producer(void *vargp){						// Producer Thread Function
 
 void *consumer(void *vargp){						// Consumer Function
 	int my = (int)vargp;
-    int count1, count2, count3 = 0;
-	while(1){
+    int count1_h, count1_f, count1_s, count2_h, count2_f, count2_s, count3_h, count3_f, count3_s = 0;
+
+	printf("Choice %d %d.\n", choice[0], quantity[0]);
+	printf("Choice %d %d.\n\n", choice[1], quantity[1]);
+	while(1) {
 		pthread_mutex_lock(&mutex);		// Check if resource is free.
-		if(buffer > 0){					// Check if the buffer is not full.
-			buffer--;
-            if (my == 0)
-		{
-			count1++;
-		}
-		else if (my == 1)
-		{
-			count2++;
-		}
-		else if (my == 2)
-		{
-			count3++;
-		}
-			printf("Buffer Decremented \tby Customer: %d \tCurrent Size: %d\n",(my+1),buffer );
-			// sleep(1);
-		}
-		else{
-			// printf("Buffer UNDERFLOW \t\t\tCurrent Size: %d\n",buffer );
+		if(quantity[0] > 0 && quantity[1] > 0) {
+			if(choice[0] == 0 || choice[1] == 0) { // hamburger
+				if(choice[0] == 0)
+					quantity[0]--;
+				else
+					quantity[1]--;
+				if(my == 1)
+					count1_h++;				
+				else if(my == 2)
+					count2_h++;
+			} else if(choice[0] == 1 || choice[1] == 1) { // fries
+				if(choice[0] == 1)
+					quantity[0]--;
+				else
+					quantity[1]--;
+				if(my == 0)
+					count1_f++;				
+				else if(my == 2)
+					count2_f++;
+			} else { // soda
+				if(choice[0] == 2)
+					quantity[0]--;
+				else
+					quantity[1]--;
+				if(my == 0)
+					count1_s++;				
+				else if(my == 1)
+					count2_s++;
+			}
 		}
 		pthread_mutex_unlock(&mutex);	// free the resource.
-    printf("Customer 1 ate: %d\n\n", count1);
-	printf("Customer 2 ate: %d\n\n", count2);
-	printf("Customer 3 ate: %d\n\n", count3);
+		printf("\n\nCustomer 1 ate: %d hamburgers, %d fries, and %d soda.\n", count1_h, count1_f, count1_s);
+		printf("Customer 2 ate: %d hamburgers, %d fries, and %d soda.\n", count2_h, count2_f, count2_s);
+		printf("Customer 3 ate: %d hamburgers, %d fries, and %d soda.\n", count3_h, count3_f, count3_s);
 	}
 	return NULL;
 }
